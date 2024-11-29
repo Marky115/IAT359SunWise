ca00495fea7086a8928f0de15394196f8a8170fb
w3nnn <95375266+w3nnn@users.noreply.github.com>
2024-11-28 19:06:34 -0800

    updating map size

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -362,7 +362,7 @@ const styles = StyleSheet.create({
   },
   mapContainer: {
     width: '100%',
-    height: '60%',
+    height: '70%',
     borderRadius: 20,
     overflow: 'hidden',
     marginTop: 40,
