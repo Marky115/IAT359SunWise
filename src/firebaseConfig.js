0cd36c887030af33e7182d91cb0fea55a907fcdb
Mark <>
2024-12-02 18:23:36 -0800

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,11 +8,11 @@ import { getFirestore } from "firebase/firestore";  // For Firestore
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyD3uEQt_mZV6js44IizaHjGK8kLhjTwsXU",
-  authDomain: "iat359-lab6.firebaseapp.com",
-  projectId: "iat359-lab6",
-  storageBucket: "iat359-lab6.appspot.com",
-  messagingSenderId: "326960630710",
-  appId: "1:326960630710:web:ede4221489c8d7297c0f17"
+    authDomain: "iat359-lab6.firebaseapp.com",
+    projectId: "iat359-lab6",
+    storageBucket: "iat359-lab6.appspot.com",
+    messagingSenderId: "326960630710",
+    appId: "1:326960630710:web:ede4221489c8d7297c0f17"
 };
 
 // Initialize Firebase
