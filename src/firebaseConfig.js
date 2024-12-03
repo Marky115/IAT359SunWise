42840927f1824ec59cc90223cc31d37f4b8ab9a7
Mark <>
2024-12-03 12:05:18 -0800

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,7 @@
 import { initializeApp } from "firebase/app";
-import AsyncStorage from '@react-native-async-storage/async-storage'; // Correct import
-import { getAuth, initializeAuth, getReactNativePersistence  } from "firebase/auth";  // For Firebase auth
-import { getFirestore } from "firebase/firestore";  // For Firestore
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import { getAuth, initializeAuth, getReactNativePersistence  } from "firebase/auth";  
+import { getFirestore } from "firebase/firestore";  
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
